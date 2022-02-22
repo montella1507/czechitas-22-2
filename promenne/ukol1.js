d594e916d5b0cd6fdc1eef2691204a92c867bc98
montella1507 <marian.bencat@live.com>
2022-02-22 17:41:04 +0100

    1 file modified

--- promenne/ukol1.js
+++ promenne/ukol1.js
@@ -1,1 +1,24 @@
-console.log("dqddq");
+
+
+// K vypsání do konzole
+console.log("lqdjwkldjqld dlkqwdj"); // do konce řádky
+/*
+dqwd
+wqd
+qwd
+wqdwqd
+wqddqw
+dqwddqw
+dqwddqwdqw
+d
+wqdqwd
+
+*/ 
+  
+
+/*
+  více
+  řádkový
+  komentář
+
+*/
